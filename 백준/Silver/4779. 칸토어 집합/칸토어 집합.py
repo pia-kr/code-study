f72fff33aebbb10a1ff7d1d95bd952e132c83d08
pia-kr <102415438+pia-kr@users.noreply.github.com>
2026-03-16 16:16:02 +0900

    [Silver III] Title: 칸토어 집합, Time: 36 ms, Memory: 33796 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -3,6 +3,7 @@ def f(n):
     if n == 1:
         return "-"
     else:
+        # 3등분
         a = f(n // 3)
         b = " " * (n // 3)
         return a + b + a
@@ -11,7 +12,7 @@ while True:
     try:
         # 입력받기
         N = int(input())
-        ans = f(3 ** N)
-        print(ans)
+        # 수정2 : 그냥 줄여도 됨
+        print(f(3 ** N))
     except:
         break
