738820c4786ee943b47f1c8ffb3c90a1f8efb3c1
pia-kr <102415438+pia-kr@users.noreply.github.com>
2026-03-17 15:11:09 +0900

    [Silver III] Title: 칸토어 집합, Time: 40 ms, Memory: 33796 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -1,18 +1,17 @@
-# 반복할 함수 만들기
-def f(n):
-    if n == 1:
-        return "-"
-    else:
-        # 3등분
-        a = f(n // 3)
-        b = " " * (n // 3)
-        return a + b + a
-# 계속 반복문 / 수정: 파일의 반복 끝 -> try/except으로
-while True:
+# 최종 수정본
+def f(N):
+    # 0을 고려 안하는 이유 : 3^N으로 받기에 최솟값이 1
+    if N == 1:
+        return '-'
+    # 수정 : 변수 할당 굳이 하지말고 바로 하기
+    else :
+        return f(N//3) +' '*(N//3) + f(N//3)        
+
+while(True):
     try:
         # 입력받기
         N = int(input())
-        # 수정2 : 그냥 줄여도 됨
-        print(f(3 ** N))
+        # N이 1이면 길이가 3짜리를 - - 모양으로 해야하기에 3의 N 제곱
+        print(f(3**N))
     except:
         break
