bf1c15c0488eba31854a8b37da261a533d65411b
pia-kr <102415438+pia-kr@users.noreply.github.com>
2026-03-17 13:23:29 +0900

    [Silver V] Title: 숫자 카드, Time: 696 ms, Memory: 117260 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,19 +1,16 @@
-# 수정사항 : 빠른 입력을 위한 sys 이용 수정3: 리스트도 빠른 입력
 import sys
+
 N = int(sys.stdin.readline())
-N_list = list(map(int , sys.stdin.readline().split()))
-
+cards = list(map(int, sys.stdin.readline().split()))
 M = int(sys.stdin.readline())
-M_list = list(map(int , sys.stdin.readline().split()))
+checks = list(map(int, sys.stdin.readline().split()))
 
-# 수정2 : 출력을 배열 만들고 출력말고 바로 출력
-# 수정4 : 배열 출력말고 딕셔너리를 출력하면 더 빠름
-dict_ans = {}
-for i in range(len(N_list)):
-    dict_ans[N_list[i]] = 0
+_dict = {}  # 속도는 dictionary!
+for i in range(len(cards)):
+    _dict[cards[i]] = 0  # 아무 숫자로 mapping
 
 for j in range(M):
-    if M_list[j] in dict_ans:
-        print(1, end = ' ')
+    if checks[j] not in _dict:
+        print(0, end=' ')
     else:
-        print(0, end = ' ')
+        print(1, end=' ')
