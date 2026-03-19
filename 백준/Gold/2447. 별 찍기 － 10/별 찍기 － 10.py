90b1700e738cf16558a8092e7141e1eb627ee5f1
pia-kr <102415438+pia-kr@users.noreply.github.com>
2026-03-19 10:33:30 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 44 ms, Memory: 41760 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,19 +1,24 @@
-# 함수 선언
-def f(n):
-    # 초기
-    if n == 1:
+# 최종 정리 버전 - 배열 안에 배열로
+def f(N):
+    # 배열을 반환시키기
+    if N == 1:
         return ["*"]
-    a = f( n // 3)
+    # 9*9 배열이면 그 각 칸 안에는 3*3 일때의 f(n)이 들어가면 됨
+    a = f(N//3)
+    # 정답 배열 넣을 배열
     ans = []
-
+    # 1, 2, 3번째
     for i in a:
-        ans.append( i * 3)
+        ans.append(i + i + i)
+    # 4, 5, 6번째 -> * 5번째는 빈칸임
     for i in a:
-        ans.append( i + " " * (n // 3) + i)
+        ans.append(i + " "*(N//3) + i)
+    # 7, 8, 9번째 배열
     for i in a:
-        ans.append( i * 3)
-
+        ans.append(i + i + i)
     return ans
-# N 입력받아서 출력하기
+
+# N 입력받기
 N = int(input())
+# 배열을 연달아 출력해주는 join
 print("\n".join(f(N)))
