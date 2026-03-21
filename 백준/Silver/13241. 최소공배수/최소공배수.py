67952dba34c8c0e5a64529557881220a3908df61
pia-kr <102415438+pia-kr@users.noreply.github.com>
2026-03-21 23:15:02 +0900

    [Silver V] Title: 최소공배수, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.py
@@ -0,0 +1,14 @@
+# 생각 : 두 수를 입력 받음 
+# -> 그 수 중 큰 수를 기준으로 배수화 시키다가 작은 수로 나누어서 나머지가 0인 수 찾기
+A, B = map(int, input().split())
+
+if A < B :
+    A, B = B, A
+count = 1
+ans = A
+while(True):
+    if ans % B == 0:
+        break
+    count += 1
+    ans = A * count
+print(ans)
